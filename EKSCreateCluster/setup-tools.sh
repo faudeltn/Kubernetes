5e2609186135b7fa07a3dc2c232c816020e4c3ec
Lotfi Waderni <waderni.lotfi@gmail.com>
2022-01-18 16:13:24 +0100

    Update setup-tools.sh

--- EKSCreateCluster/setup-tools.sh
+++ EKSCreateCluster/setup-tools.sh
@@ -1,3 +1,21 @@
+echo "Install OS tools"
+sudo yum -y -q -e 0 install  jq moreutils nmap > /dev/null
+echo "Update OS tools"
+sudo yum update -y > /dev/null
+echo "Update pip"
+sudo pip install --upgrade pip 2&> /dev/null
+
+#
+echo "Uninstall AWS CLI v1"
+sudo /usr/local/bin/pip uninstall awscli -y 2&> /dev/null
+sudo pip uninstall awscli -y 2&> /dev/null
+echo "Install AWS CLI v2"
+curl --silent "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" > /dev/null
+unzip -qq awscliv2.zip
+sudo ./aws/install > /dev/null
+rm -f awscliv2.zip
+rm -rf aws
+
 echo "Setup kubectl"
 if [ ! `which kubectl 2> /dev/null` ]; then
   echo "Install kubectl"
@@ -20,7 +38,7 @@ fi
 if [ ! `which helm 2> /dev/null` ]; then
   echo "install helm 3"
   export VERIFY_CHECKSUM=false
-  curl -fsSL https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
+  curl -fsSL https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash > /dev/null
 fi
 echo "add helm repos"
 helm repo add eks https://aws.github.io/eks-charts
